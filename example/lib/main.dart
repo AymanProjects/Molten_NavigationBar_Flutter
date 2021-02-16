66476fe2f35e4382d07815231f0e004afac1236f
Ayman <Aymanz.dev@gmail.com>
2021-02-16 20:55:40 +0300

    deleted old example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:example/examples/example_1.dart';
-import 'package:example/examples/example_2.dart';
 import 'package:flutter/material.dart';
+import 'package:molten_navigationbar_flutter/molten_navigationbar_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,8 +11,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // a variable to store the current selected tab. can be used to control PageView
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Example1();
+    return MaterialApp(
+      theme: Theme.of(context).copyWith(primaryColor: Colors.deepPurple[400]),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Selected Tab: $_selectedIndex',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.deepPurple[400],
+        // you can use the molten bar in the scaffold's bottomNavigationBar
+        bottomNavigationBar: MoltenBottomNavigationBar(
+          selectedIndex: _selectedIndex,
+          // specify what will happen when a tab is clicked
+          onTabChange: (clickedIndex) {
+            setState(() {
+              _selectedIndex = clickedIndex;
+            });
+          },
+          // ansert as many tabs as you like
+          tabs: [
+            MoltenTab(
+              icon: Icon(Icons.search),
+              selectedColor: Colors.yellow,
+            ),
+            MoltenTab(
+              icon: Icon(Icons.home),
+              selectedColor: Colors.yellow,
+            ),
+            MoltenTab(
+              icon: Icon(Icons.person),
+              selectedColor: Colors.yellow,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
